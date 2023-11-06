0395b9ffb04a2bbc44dddd72fccbad8daaca8320
v.ghadiri <v.ghadiri@sdb247.com>
2023-11-06 18:08:44 +0330

    feat(config): Add error-boundary-provider

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
 export { default as QueryProvider } from "./providers/react-query";
-export {default as LanguageProvider} from "./providers/language"
+export { default as LanguageProvider } from "./providers/language";
+export { default as ErrorBoundaryProvider } from "./providers/error-boundary";
---
+++ src/config/providers/error-boundary.tsx
@@ -0,0 +1,44 @@
+import type { PropsWithChildren, ReactNode } from "react";
+import React from "react";
+import { GENERAL_CONSTANTS } from "../../constants";
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+const FallbackComponent = () => {
+  const onReset = () => (window.location.href = window.location.origin);
+  return <div onClick={onReset}>{GENERAL_CONSTANTS.RUNTIME_ERROR_MESSAGE}</div>;
+};
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  static componentDidCatch(error: Error) {
+    console.log("runtime-error:", error);
+  }
+
+  render() {
+    return this.state.hasError ? <FallbackComponent /> : this.props.children;
+  }
+}
+
+const ErrorBoundaryProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  return <ErrorBoundary>{children}</ErrorBoundary>;
+};
+
+export default ErrorBoundaryProvider;
