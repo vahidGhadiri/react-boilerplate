16be123f2d55975d4137471829b2197fe9e7c3fa
v.ghadiri <v.ghadiri@sdb247.com>
2023-11-06 17:48:36 +0330

    feat(config): Use languge-provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { QueryProvider } from "./config";
+import LanguageProvider from "./config/providers/language";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <QueryProvider>
-    <div>Render inital commit</div>
-  </QueryProvider>
+  <LanguageProvider>
+    <QueryProvider>
+      <div>Render inital commit</div>
+    </QueryProvider>
+  </LanguageProvider>
 );
