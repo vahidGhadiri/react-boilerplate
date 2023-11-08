699f714bd591a5a5bc63f284ed6e445aaca93e94
v.ghadiri <v.ghadiri@sdb247.com>
2023-11-08 14:43:40 +0330

    feat(config/infrastructure): Add http file to handle network requests

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export { default as QueryProvider } from "./providers/react-query";
 export { default as LanguageProvider } from "./providers/language";
 export { default as ErrorBoundaryProvider } from "./providers/error-boundary";
+export { default as Http } from "./infrastructures/http";
